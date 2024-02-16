6a0825fc88d817075a1b4b05190c99c216b832bf
quarbis <115672837+quarbis@users.noreply.github.com>
2024-02-16 17:37:03 +0000

    cleaned up code and added display_cards() function

--- Blackjack.py
+++ Blackjack.py
@@ -1,26 +1,54 @@
 import random
 
-cards = list(range(2, 11)) + ["Jack", "King", "Queen", "Ace"]
-signs = ["hearts", "spades", "clubs", "diamonds"]
+
+card_values = list(range(2, 11)) + ["jack", "king", "queen", "ace"]
+suits = ["hearts", "spades", "clubs", "diamonds"]
+
+cards = [(card, suit) for card in card_values for suit in suits]
 
 def deal_card():
-    return str(cards[random.randint(0, 12)]) + " of " + signs[random.randint(0, 3)]
+    card, suit = random.choice(cards)
+    return str(card) + " of " + suit
     
+def display_cards(dealer_cards, player_cards):
+    player_hand = "You have "
+    for i in range(len(player_cards)):
+        player_hand += "a " + player_cards[i]
+        if i >= len(player_cards) - 2 and len(player_cards) > 2:
+            player_hand += ","
+        player_hand += " "
+        if i == len(player_cards) - 2:
+            player_hand += "and "
+    dealer_hand = "The dealer has "
+    for i in range(len(dealer_cards)):
+        dealer_hand += "a " + dealer_cards[i]
+        if i < len(dealer_cards) - 1 and len(dealer_cards) > 2:
+            dealer_hand += ","
+        dealer_hand += " "
+        if i == len(dealer_cards) - 2:
+            dealer_hand += "and "
+    print(player_hand)
+    print(dealer_hand)
+    
+
 def round(bank):
     print("You have ${}".format(bank))
-    bet = input("How much do you want to bet?\n")
-
-    def bet_to_int(bet):
-        try:
-            bet = int(bet)
-            return True
-        except ValueError:
-            return False
-
-    while not bet_to_int(bet) or int(bet) < 1 or int(bet) > bank:
-        print("Invalid amount")
-        bet = input("Try again: ")
+    
+    while True:
+        bet = input("How much do you want to bet?\n")
+        if bet.isdigit() and 1 <= int(bet) <= bank:
+            break
+        else:
+            print("Invalid amount")
 
     print("You bet {} dollars".format(bet))
+
+    player_cards = [deal_card(), deal_card()]
+    
+    dealer_cards = [deal_card(), deal_card()]
+
+
+    display_cards(player_cards, dealer_cards)
+
 
 round(20)
