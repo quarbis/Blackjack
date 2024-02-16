f1399f03e6f186745934b4465c0934f7e929cf14
quarbis <115672837+quarbis@users.noreply.github.com>
2024-02-16 17:16:10 +0000

    created card dealing function and a bet requesting function

--- Blackjack.py
+++ Blackjack.py
@@ -1,2 +1,26 @@
-print(1+1)
-print("teehee")
+import random
+
+cards = list(range(2, 11)) + ["Jack", "King", "Queen", "Ace"]
+signs = ["hearts", "spades", "clubs", "diamonds"]
+
+def deal_card():
+    return str(cards[random.randint(0, 12)]) + " of " + signs[random.randint(0, 3)]
+    
+def round(bank):
+    print("You have ${}".format(bank))
+    bet = input("How much do you want to bet?\n")
+
+    def bet_to_int(bet):
+        try:
+            bet = int(bet)
+            return True
+        except ValueError:
+            return False
+
+    while not bet_to_int(bet) or int(bet) < 1 or int(bet) > bank:
+        print("Invalid amount")
+        bet = input("Try again: ")
+
+    print("You bet {} dollars".format(bet))
+
+round(20)
