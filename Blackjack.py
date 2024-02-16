e2dee458fd6eee478ecb49f1594332da211647ec
quarbis <115672837+quarbis@users.noreply.github.com>
2024-02-16 19:35:53 +0000

    added a function that evaluates choices and one that prints choices

--- Blackjack.py
+++ Blackjack.py
@@ -8,12 +8,13 @@ cards = [(card, suit) for card in card_values for suit in suits]
 
 def deal_card():
     card, suit = random.choice(cards)
-    return str(card) + " of " + suit
+    return [card, suit]
     
 def display_cards(dealer_cards, player_cards):
+    print()
     player_hand = "You have "
     for i in range(len(player_cards)):
-        player_hand += "a " + player_cards[i]
+        player_hand += "a " + str(player_cards[i][0]) + " of " + player_cards[i][1]
         if i >= len(player_cards) - 2 and len(player_cards) > 2:
             player_hand += ","
         player_hand += " "
@@ -21,7 +22,7 @@ def display_cards(dealer_cards, player_cards):
             player_hand += "and "
     dealer_hand = "The dealer has "
     for i in range(len(dealer_cards)):
-        dealer_hand += "a " + dealer_cards[i]
+        dealer_hand += "a " + str(player_cards[i][0]) + " of " + player_cards[i][1]
         if i < len(dealer_cards) - 1 and len(dealer_cards) > 2:
             dealer_hand += ","
         dealer_hand += " "
@@ -30,6 +31,22 @@ def display_cards(dealer_cards, player_cards):
     print(player_hand)
     print(dealer_hand)
     
+
+def choices(dealer_cards, player_cards):
+    choices = {"S": "stand", "H": "hit"}
+    if len(player_cards) == 2:
+        choices["D"] = "double"
+    if dealer_cards[1][0] == "ace":
+        choices["I"] = "insurance"
+    return choices
+    
+def print_choices(dealer_cards, player_cards):
+    print()
+    print("Press: ")
+    choice_list = choices(dealer_cards, player_cards)
+    for letter, move in choice_list.items():
+        print(letter + " for " + move)
+    print()
 
 def round(bank):
     print("You have ${}".format(bank))
@@ -47,8 +64,15 @@ def round(bank):
     
     dealer_cards = [deal_card(), deal_card()]
 
+    display_cards(dealer_cards, player_cards)
 
-    display_cards(player_cards, dealer_cards)
+    print_choices(dealer_cards, player_cards)
 
+    while True:
+        player_move = input()
+        if player_move in choices(dealer_cards, player_cards).keys():
+            break
+        else:
+            print("invalid input, try again:")
+        
 
-round(20)
